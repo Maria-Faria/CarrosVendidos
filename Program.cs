9682da8b9ef1f2f4ca08f323d95fe32dfc3a1859
Maria-Faria <mariaeduardadefaria15@gmail.com>
2024-02-06 08:37:16 -0300

    criando o dicionário de carros

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿//criar dicionário de carros e quantidade de vendas
+Dictionary<string, List<int>> vendasCarros = new Dictionary<string, List<int>> {
+    {"Bugatti Veyron", new List<int>{ 10, 15, 12, 8, 5}},
+    {"Koenigsegg Agera RS", new List<int>{2, 3, 5, 6, 7}},
+    {"Lamborghini Aventador", new List<int>{20, 18, 22, 24, 16}},
+    {"Pagani Huayra", new List<int>{4, 5, 6, 5, 4}},
+    {"Ferrari LaFerrari", new List<int>{7, 6, 5, 8, 10}}
+};
